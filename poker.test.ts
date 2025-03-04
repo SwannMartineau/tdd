f9eaead4e81ce8bb33d982a07ec7deff4b68b25e
SwannMartineau <swann.martineau@gmail.com>
2025-03-04 15:00:30 +0100

    feat: add tests for deck integrity

--- poker.test.ts
+++ poker.test.ts
@@ -1,8 +1,22 @@
-import { cards } from "./poker";
-import {describe, it, expect} from '@jest/globals';
+import { initDeck, Card, Rank, Color } from "./poker";
+import { describe, it, expect } from '@jest/globals';
 
 describe('verify the cards', () => {
-    it('Verify that there is 54 cards in the deck', () =>  {
-        expect(cards()).toEqual(52);
+
+  it('Verify that there are 52 cards in the deck', () => {
+    expect(initDeck().length).toEqual(52);
+  });
+
+  it('Verify that all the cards are present in the deck', () => {
+    const deck = initDeck();
+    const colors: Color[] = ['Cœur', 'Carreau', 'Trèfle', 'Pique'];
+    const ranks: Rank[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As'];
+
+    colors.forEach(color => {
+      ranks.forEach(rank => {
+        const card: Card = { rank, color };
+        expect(deck).toContainEqual(card);
+      });
     });
+  });
 });
--- poker.ts
+++ poker.ts
@@ -1,3 +1,22 @@
-export function cards() {
-  return 52; 
+export type Rank = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'Valet' | 'Dame' | 'Roi' | 'As';
+export type Color = 'Cœur' | 'Carreau' | 'Trèfle' | 'Pique';
+
+export interface Card {
+  rank: Rank;
+  color: Color;
+}
+
+export function initDeck(): Card[] {
+  const colors: Color[] = ['Cœur', 'Carreau', 'Trèfle', 'Pique'];
+  const ranks: Rank[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As'];
+  
+  let deck: Card[] = [];
+  
+  colors.forEach(color => {
+    ranks.forEach(rank => {
+      deck.push({ rank, color });
+    });
+  });
+  
+  return deck;
 }
