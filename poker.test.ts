cf7223d050e34d1232f0a644cdd2c0d95c36e2a7
SwannMartineau <swann.martineau@gmail.com>
2025-03-04 15:15:31 +0100

    feat: add tests for shuffle deck

--- poker.test.ts
+++ poker.test.ts
@@ -1,4 +1,4 @@
-import { initDeck, Card, Rank, Color } from "./poker";
+import { initDeck, Card, ranks, colors, shuffleDeck } from "./poker";
 import { describe, it, expect } from '@jest/globals';
 
 describe('verify the cards', () => {
@@ -9,8 +9,6 @@ describe('verify the cards', () => {
 
   it('Verify that all the cards are present in the deck', () => {
     const deck = initDeck();
-    const colors: Color[] = ['Cœur', 'Carreau', 'Trèfle', 'Pique'];
-    const ranks: Rank[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As'];
 
     colors.forEach(color => {
       ranks.forEach(rank => {
@@ -19,4 +17,33 @@ describe('verify the cards', () => {
       });
     });
   });
+
+  it('Verify that the deck is shuffled', () => {
+    const originalDeck = initDeck();
+    const shuffledDeck = shuffleDeck(originalDeck);
+    
+    expect(shuffledDeck).not.toEqual(originalDeck);
+    
+    let isDifferent = false;
+    for (let i = 0; i < originalDeck.length; i++) {
+      if (originalDeck[i] !== shuffledDeck[i]) {
+        isDifferent = true;
+        break;
+      }
+    }
+    expect(isDifferent).toBe(true);
+  });
+
+  it('Verify that the deck is complete after shuffle', () => {
+    const deck = initDeck();
+    const shuffledDeck = shuffleDeck(deck);
+
+    colors.forEach(color => {
+      ranks.forEach(rank => {
+        const card: Card = { rank, color };
+        expect(shuffledDeck).toContainEqual(card);
+      });
+    });
+  })
+
 });
--- poker.ts
+++ poker.ts
@@ -6,10 +6,10 @@ export interface Card {
   color: Color;
 }
 
-export function initDeck(): Card[] {
-  const colors: Color[] = ['Cœur', 'Carreau', 'Trèfle', 'Pique'];
-  const ranks: Rank[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As'];
-  
+export const colors: Color[] = ['Cœur', 'Carreau', 'Trèfle', 'Pique'];
+export const ranks: Rank[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As'];
+
+export function initDeck(): Card[] {  
   let deck: Card[] = [];
   
   colors.forEach(color => {
@@ -19,4 +19,15 @@ export function initDeck(): Card[] {
   });
   
   return deck;
+}
+
+export function shuffleDeck(deck: Card[]): Card[] {
+  const shuffledDeck = [...deck]; 
+
+  for (let i = shuffledDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+  }
+
+  return shuffledDeck;
 }
